00f46821cff19820574f6df3c07bb1f58d8dadf0
ciprian96 <ciprian.daniel.turca@gmail.com>
2019-04-09 20:49:35 +0300

    End of section 13

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -5,6 +5,7 @@ import { reducer as FormReducer } from "redux-form";
 import modalsReducer from "../../features/modals/modalReducer";
 import authReducer from "../../features/auth/authReducer";
 import asyncReducer from "../../features/async/asyncReducer";
+import { reducer as toastrReducer } from "react-redux-toastr";
 
 const rootReducer = combineReducers({
   form: FormReducer,
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
   events: eventReducer,
   modals: modalsReducer,
   auth: authReducer,
-  async: asyncReducer
+  async: asyncReducer,
+  toastr: toastrReducer
 });
 
 export default rootReducer;
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import format from "date-fns/format";
 
 class EventListItem extends Component {
   render() {
@@ -28,7 +29,8 @@ class EventListItem extends Component {
         </Segment>
         <Segment>
           <span>
-            <Icon name="clock" /> {event.date} |
+            <Icon name="clock" /> {format(event.date, "dddd Do MMMM")} at{" "}
+            {format(event.date, "HH:mm")} |
             <Icon name="marker" /> {event.venue}
           </span>
         </Segment>
